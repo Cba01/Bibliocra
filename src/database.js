f031d893daf53ecabd048bba95db1b86f5ec37e6
Benjamin Valenzuela <benjamin.valenzuelaem@gmail.com>
2022-11-27 13:51:46 -0300

    algunos cambios, se agrego footer

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 
 (async () => {
   try {
-    const db = await connect("mongodb://localhost:27017/bibliocraDB");
+    const db = await connect("mongodb://127.0.0.1:27017/bibliocraDB");
     console.log("DB connect to", db.connection.name);
   } catch (error) {
     console.error(error);
