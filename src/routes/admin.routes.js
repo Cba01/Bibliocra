ce8d6dcd404e733f364d1056c5904fc1c5c2d309
Cba01 <seba.j.os@hotmail.com>
2022-12-02 19:48:37 -0300

    Agregadas tabs admin navbar

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,6 +1,6 @@
 import { response, Router } from "express";
 
-const axios = require('axios');
+const axios = require("axios");
 const router = Router();
 
 router.get("/admin", (req, res) => {
@@ -8,15 +8,25 @@ router.get("/admin", (req, res) => {
 });
 
 router.get("/admin/books", (req, res) => {
-  axios.get('http://localhost:3000/api/book/render').then((data)=>{
+  axios.get("http://localhost:3000/api/book/render").then((data) => {
     const allbooks = data.data;
-    console.log(allbooks)
-    res.render("booksAdmin", { layout: "adminLayout", books: true, allbooks})
-  })
-})
+    console.log(allbooks);
+    res.render("booksAdmin", { layout: "adminLayout", books: true, allbooks });
+  });
+});
 
-router.get("/admin/booksadd", (req, res)=>{
-    res.render("booksAdmin", { layout: "adminLayout", add: true})
-})
+router.get("/admin/booksadd", (req, res) => {
+  res.render("booksAdmin", { layout: "adminLayout", add: true });
+});
+
+router.get("/admin/users", (req, res) => {
+    res.render("usersAdmin", { layout: "adminLayout" });
+
+});
+
+router.get("/admin/sales", (req, res) => {
+  res.render("salesAdmin", { layout: "adminLayout" });
+
+});
 
 export default router;
