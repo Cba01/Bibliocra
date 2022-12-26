0bdaed37498ee37b33a82dc003cd987b28d39e02
Cba01 <seba.j.os@hotmail.com>
2022-12-26 16:26:07 -0300

    añadido diseño navbar del index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,6 +6,10 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/books", (req, res) => {
+  res.render("books");
+});
+
 router.get("/login", (req, res) => {
   res.render("auth/login", {layout: "main"});
 });
